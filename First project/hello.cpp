3c978a54d2dc318135eb72de23ea748633af1b07
joydeep-mukherjee <jmukherj@yorku.ca>
2020-12-28 12:33:13 -0500

    first project commit

---
+++ First project/hello.cpp
@@ -0,0 +1,16 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: Dec. 28, 2020
+ *      Author: Joydeep
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "Hello, world!" << endl;
+   return 0;
+}
+
+
+
